cc1a44bda6477a8e68014fc51e207f2c1f4e5ca4
tresor nkamba <showtec040@gmail.com>
2025-07-01 19:34:37 +0100

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Notification = require('../models/Notification');
+const Notification = require('../models/notification');
 
 router.get('/', async (req, res) => {
   const userId = req.query.userId;
