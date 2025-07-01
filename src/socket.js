d067e332ddc6f8e0d36b927ad6b132348ae9eb76
tresor nkamba <showtec040@gmail.com>
2025-07-01 17:03:03 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,7 @@
-
 const { Server } = require('socket.io');
 // Stockage temporaire (remplace par une base de données si besoin)
-let messages = []; // [{from, to, message, date}]
-let userSockets = {}; // { nomUtilisateur: socket.id }
+let messages = []; // [{fromId, toId, message, date}]
+let userSockets = {}; // { userId: socket.id }
 
 function setupSocket(server) {
     const io = new Server(server, {
@@ -12,17 +11,17 @@ function setupSocket(server) {
     io.on('connection', (socket) => {
         console.log('Nouvel utilisateur connecté');
 
-        // Enregistrement de l'utilisateur
-        socket.on('register', (nomUtilisateur) => {
-            userSockets[nomUtilisateur] = socket.id;
+        // Enregistrement de l'utilisateur par son _id
+        socket.on('register', (userId) => {
+            userSockets[userId] = socket.id;
         });
 
         // Quand le client demande l'historique pour une conversation
         socket.on('demanderHistorique', ({ userA, userB }) => {
             const historique = messages.filter(
                 m =>
-                    (m.from === userA && m.to === userB) ||
-                    (m.from === userB && m.to === userA)
+                    (m.fromId === userA && m.toId === userB) ||
+                    (m.fromId === userB && m.toId === userA)
             );
             socket.emit('messageHistory', historique);
         });
@@ -32,20 +31,20 @@ function setupSocket(server) {
             data.date = new Date();
             messages.push(data);
             // Envoi à l'expéditeur
-            if (userSockets[data.from]) {
-                io.to(userSockets[data.from]).emit('receiveMessage', data);
+            if (userSockets[data.fromId]) {
+                io.to(userSockets[data.fromId]).emit('receiveMessage', data);
             }
             // Envoi au destinataire
-            if (userSockets[data.to] && data.to !== data.from) {
-                io.to(userSockets[data.to]).emit('receiveMessage', data);
+            if (userSockets[data.toId] && data.toId !== data.fromId) {
+                io.to(userSockets[data.toId]).emit('receiveMessage', data);
             }
         });
 
         socket.on('disconnect', () => {
             // Nettoyage (optionnel)
-            for (const [user, id] of Object.entries(userSockets)) {
+            for (const [userId, id] of Object.entries(userSockets)) {
                 if (id === socket.id) {
-                    delete userSockets[user];
+                    delete userSockets[userId];
                     break;
                 }
             }
