cd42fb3c7a5f0b62ef053d05759dbd329225673f
tresor nkamba <showtec040@gmail.com>
2025-07-20 12:10:29 +0100

    Update agentController.js

--- src/controllers/agentController.js
+++ src/controllers/agentController.js
@@ -114,19 +114,21 @@ exports.createAgent = async (req, res) => {
         });
         await nouvelAgent.save();
 
-// --- AJOUT : Notification pour SG et DRH ---
-        const fonctionsNotif = ["Directeur des ressources humaines", "Secrétaire Général"];
-        const destinataires = await Agent.find({ fonction: { $in: fonctionsNotif } });
-        for (const destinataire of destinataires) {
-            await Notification.create({
-                userId: destinataire._id,
-                titre: "Nouvel agent ajouté",
-                message: `Un nouvel agent (${nouvelAgent.nomComplet}) vient d'être ajouté.`,
-                date: new Date(),
-                lu: false
-            });
-        }
-        
+// --- AJOUT : Notification pour Directeur Chef de services et Secrétaire de Direction du même département ---
+const fonctionsNotif = ["Directeur Chef de services", "Secrétaire de Direction"];
+const destinataires = await Agent.find({
+  fonction: { $in: fonctionsNotif },
+  departement: nouvelAgent.departement
+});
+for (const destinataire of destinataires) {
+  await Notification.create({
+    userId: destinataire._id,
+    titre: "Nouvel agent ajouté",
+    message: `Un nouvel agent (${nouvelAgent.nomComplet}) vient d'être ajouté dans votre département.`,
+    date: new Date(),
+    lu: false
+  });
+}
         res.json({ success: true, statut: nouvelAgent.statut, numeroInscription: nouvelAgent.numeroInscription });
     } catch (err) {
         res.status(400).json({ success: false, message: err.message });
