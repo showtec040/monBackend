81a190cc6afa044cb651d34c7c31e771a1013be9
tresor nkamba <showtec040@gmail.com>
2025-08-07 06:55:15 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,30 +1,12 @@
 // socket.js
 const { Server } = require('socket.io');
 const Message = require('./models/Message'); // adapte le chemin si besoin
-<<<<<<< HEAD
-
-// Pour garder la liste des utilisateurs connectés
-const connectedUsers = new Map();
-=======
->>>>>>> d8273cb (Mise à jour backend : sécurité, email, corrections)
 
 // Pour garder la liste des utilisateurs connectés
 const connectedUsers = new Map();
 
 let ioInstance = null;
 function setupSocket(server) {
-<<<<<<< HEAD
-    const io = new Server(server, {
-        cors: { origin: "*", methods: ["GET", "POST"] }
-    });
-
-    io.on('connection', (socket) => {
-        // Authentification simple
-        socket.on('login', ({ userId }) => {
-            connectedUsers.set(socket.id, userId);
-            const onlineUserIds = Array.from(connectedUsers.values());
-            io.emit('onlineUsers', onlineUserIds);
-=======
     ioInstance = new Server(server, {
         cors: { origin: "*", methods: ["GET", "POST"] }
     });
@@ -37,7 +19,6 @@ function setupSocket(server) {
             socket.join(userId);
             const onlineUserIds = Array.from(connectedUsers.values());
             ioInstance.emit('onlineUsers', onlineUserIds);
->>>>>>> d8273cb (Mise à jour backend : sécurité, email, corrections)
         });
 
         // Récupérer l'historique des messages
@@ -60,11 +41,7 @@ function setupSocket(server) {
             // Envoi au destinataire et à l'expéditeur
             for (let [sockId, userId] of connectedUsers.entries()) {
                 if (userId == msg.to || userId == msg.from) {
-<<<<<<< HEAD
-                    io.to(sockId).emit('newMessage', message);
-=======
                     ioInstance.to(sockId).emit('newMessage', message);
->>>>>>> d8273cb (Mise à jour backend : sécurité, email, corrections)
                 }
             }
         });
@@ -72,21 +49,13 @@ function setupSocket(server) {
         socket.on('disconnect', () => {
             connectedUsers.delete(socket.id);
             const onlineUserIds = Array.from(connectedUsers.values());
-<<<<<<< HEAD
-            io.emit('onlineUsers', onlineUserIds);
-=======
             ioInstance.emit('onlineUsers', onlineUserIds);
->>>>>>> d8273cb (Mise à jour backend : sécurité, email, corrections)
         });
     });
 }
 
-<<<<<<< HEAD
-module.exports = { setupSocket };
-=======
 function getIO() {
     return ioInstance;
 }
 
 module.exports = { setupSocket, getIO };
->>>>>>> d8273cb (Mise à jour backend : sécurité, email, corrections)
